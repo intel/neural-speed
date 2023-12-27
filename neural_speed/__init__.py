ec840418495d53b815a9c7a868e9f59676ca8e99
zhenwei-intel <zhenwei.liu@intel.com>
2023-12-27 10:01:29 +0800

    update
    Signed-off-by: zhenwei-intel <zhenwei.liu@intel.com>

--- neural_speed/__init__.py
+++ neural_speed/__init__.py
@@ -134,7 +134,8 @@ class Model:
         self.__import_package(model_type)
         self.module.Model.quant_model(model_path=model_path, out_path=out_path, **quant_kwargs)
 
-    def generate(self, input_ids, streamer=None, interactive=False, ignore_prompt=False, stopping_criteria=None,  **generate_kwargs):
+    def generate(self, input_ids, streamer=None, interactive=False, ignore_prompt=False,
+                 stopping_criteria=None,  **generate_kwargs):
         max_new_tokens = generate_kwargs.get("max_new_tokens", -1)
         if self.model is None:
             self.init_from_bin(self.model_type, self.bin_file, batch_size=input_ids.shape[0],
