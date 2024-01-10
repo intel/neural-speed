32757a90d04fb57bb668a3080ab4436aa7b22fd2
intellinjun <jun.lin@intel.com>
2024-01-10 16:09:02 +0800

    miagrate pr [LLM Runtime] Add Whisper Example and Python API
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- neural_speed/__init__.py
+++ neural_speed/__init__.py
@@ -62,6 +62,8 @@ class Model:
             import neural_speed.polyglot_cpp as cpp_model
         elif model_type == "mistral":
             import neural_speed.mistral_cpp as cpp_model
+        elif model_type == "whisper":
+            import neural_speed.whisper_cpp as cpp_model
         else:
             raise TypeError("Unspported model type {}!".format(model_type))
         self.module = cpp_model
@@ -225,6 +227,7 @@ class Model:
                 raise ValueError("Please set pad_token_id when doing multi batch inference"\
                                   " with padding!")
         return self.tokenizer.pad_token_id
+
 
     def __call__(self, model_input, reinit=False, **kwargs):
         if self.model_type == 'whisper':
