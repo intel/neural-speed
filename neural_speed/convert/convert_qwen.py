6c8624827b434c61573a02026b9d20f3490ab97e
intellinjun <jun.lin@intel.com>
2024-03-07 15:14:43 +0800

    fix qwen convert error
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- neural_speed/convert/convert_qwen.py
+++ neural_speed/convert/convert_qwen.py
@@ -130,12 +130,12 @@ def main(args_in: Optional[List[str]] = None) -> None:
     fout.write(struct.pack("f", 0.0))  # config.json "rope_scaling.factor", not enabled
     fout.write(struct.pack("i", 0))  # rope_scaling.original_max_position_embeddings
     fout.write(struct.pack("i", 0))  # params["rope_scaling"]["type"] =="yarn" else 0))
-    fout.write(
-        struct.pack(
-            "i", hparams["bos_token_id"] if "bos_token_id" in hparams else tokenizer.special_tokens['<|endoftext|>']))
-    fout.write(
-        struct.pack(
-            "i", hparams["eos_token_id"] if "eos_token_id" in hparams else tokenizer.special_tokens['<|endoftext|>']))
+    if hparams['model_type']=='qwen2':
+        fout.write(struct.pack("i", hparams["bos_token_id"]))
+        fout.write(struct.pack("i", hparams["eos_token_id"]))
+    else:
+        fout.write(struct.pack("i", tokenizer.special_tokens['<|endoftext|>']))
+        fout.write(struct.pack("i", tokenizer.special_tokens['<|endoftext|>']))
     fout.write(struct.pack("i", tokenizer.pad_token_id if tokenizer.pad_token_id is not None else -1))
     fout.write(struct.pack("i", tokenizer.sep_token_id if tokenizer.sep_token_id is not None else -1))
 
