5464283095cf246d1d2b930146525262ffadf754
yuchengliu <yucheng.liu@intel.com>
2024-03-29 02:28:48 -0700

    fix bug

--- bestla/bestla/kernel_wrapper.h
+++ bestla/bestla/kernel_wrapper.h
@@ -456,6 +456,8 @@ class DecompressKBlockS3Fp {
       ret = avx512f::decompress_kblock_bit3_packrow_fp<S3_T, _DST_T, _PACK_ROW, _SCA_T>(
           bit2ptr, bit1ptr, dstptr, interleave_n_offset, row, col, scales, zero_points, k_offset, kblock, NPad, tmp,
           tmpsize);
+      assert(ret == BTLA_CODE::Success);
+      return ret;
     }
 #endif
 #if CompileAVX2()
@@ -463,6 +465,8 @@ class DecompressKBlockS3Fp {
       ret = avx2::decompress_kblock_bit3_packrow_fp<S3_T, _DST_T, _PACK_ROW, _SCA_T>(
           bit2ptr, bit1ptr, dstptr, interleave_n_offset, row, col, scales, zero_points, k_offset, kblock, NPad, tmp,
           tmpsize);
+      assert(ret == BTLA_CODE::Success);
+      return ret;
     }
 #endif
     ret = ref::decompress_kblock_bit3_packrow_fp<S3_T, _DST_T, _PACK_ROW, _SCA_T>(
