64e2439399569cef7a72189c13ef2a9d949a6773
Zhenzhong1 <zhenzhong.xu@intel.com>
2024-04-11 00:49:51 -0700

    add prompt template

--- scripts/python_api_example_for_gguf.py
+++ scripts/python_api_example_for_gguf.py
@@ -18,7 +18,7 @@ import sys
 import argparse
 from pathlib import Path
 from typing import List, Optional
-from transformers import AutoTokenizer, TextStreamer
+from transformers import AutoTokenizer, TextStreamer, AutoModelForCausalLM
 from neural_speed import Model
 
 # Usage:
@@ -26,6 +26,52 @@ from neural_speed import Model
 # --model_name falcon \
 # --model_path /model_path/falcon-7b \
 # -m /model_path/falcon-7b/ggml-model-f32.gguf
+
+import torch
+def build_chat_input(model, tokenizer, messages: List[dict], max_new_tokens: int=0):
+    def _parse_messages(messages, split_role="user"):
+        system, rounds = "", []
+        round = []
+        for i, message in enumerate(messages):
+            if message["role"] == "system":
+                assert i == 0
+                system = message["content"]
+                continue
+            if message["role"] == split_role and round:
+                rounds.append(round)
+                round = []
+            round.append(message)
+        if round:
+            rounds.append(round)
+        return system, rounds
+
+    max_new_tokens = max_new_tokens or model.generation_config.max_new_tokens
+    max_input_tokens = model.config.model_max_length - max_new_tokens
+    system, rounds = _parse_messages(messages, split_role="user")
+    system_tokens = tokenizer.encode(system)
+    max_history_tokens = max_input_tokens - len(system_tokens)
+
+    history_tokens = []
+    for round in rounds[::-1]:
+        round_tokens = []
+        for message in round:
+            if message["role"] == "user":
+                round_tokens.append(model.generation_config.user_token_id)
+            else:
+                round_tokens.append(model.generation_config.assistant_token_id)
+            round_tokens.extend(tokenizer.encode(message["content"]))
+        if len(history_tokens) == 0 or len(history_tokens) + len(round_tokens) <= max_history_tokens:
+            history_tokens = round_tokens + history_tokens  # concat left
+            if len(history_tokens) < max_history_tokens:
+                continue
+        break
+
+    input_tokens = system_tokens + history_tokens
+    if messages[-1]["role"] != "assistant":
+        input_tokens.append(model.generation_config.assistant_token_id)
+    input_tokens = input_tokens[-max_input_tokens:]  # truncate left
+    return torch.LongTensor([input_tokens]).to(model.device)
+
 
 def main(args_in: Optional[List[str]] = None) -> None:
     parser = argparse.ArgumentParser(description="main program llm running")
@@ -60,15 +106,22 @@ def main(args_in: Optional[List[str]] = None) -> None:
     # model.init_from_bin(args.model_name, gguf_path)
     # outputs = model.generate(inputs, streamer=streamer, max_new_tokens=300, do_sample=True)
 
+
     # prompt = args.prompt
     tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-    import torch
-    inputs = torch.tensor([[ 195, 16829, 92361,   196]])
-    # streamer = TextStreamer(tokenizer)
+    transformer_model = AutoModelForCausalLM.from_pretrained(args.model_path, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
+    # generation_config = GenerationConfig.from_pretrained(args.model_path)
+    
+    ## inputs = torch.tensor([[ 195, 16829, 92361,   196]])
+    messages = []
+    messages.append({"role": "user", "content": "你好”"})
 
+    from transformers.generation.utils import GenerationConfig
+    inputs_ids = build_chat_input(transformer_model, tokenizer, messages, max_new_tokens = 300)
+    print(inputs_ids)
     model = Model()
     model.init_from_bin(args.model_name, gguf_path)
-    outputs = model.generate(inputs, max_new_tokens=300, do_sample=True)
+    outputs = model.generate(inputs_ids, max_new_tokens=300, do_sample=True)
     words = tokenizer.decode(outputs[0])
     print(words)
     
