71cab6d96585fb3efd7c545c98607e420b8eabd8
intellinjun <jun.lin@intel.com>
2024-04-22 09:31:05 +0800

    remove debug code
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- neural_speed/models/grok/grok.h
+++ neural_speed/models/grok/grok.h
@@ -24,12 +24,6 @@ enum grok_model {
 
 static const model_scratch grok_mem_req(int n_layers, float enlarge_scale = 1.0f) {
   switch (n_layers) {
-    case 2:
-      return {
-          static_cast<unsigned long long>(enlarge_scale * 1024) * MB,
-          static_cast<unsigned long long>(enlarge_scale * 1024) * MB,
-          static_cast<unsigned long long>(enlarge_scale * 1608) * MB,
-      };
     case 64:
       return {
           static_cast<unsigned long long>(enlarge_scale * 3072) * MB,
