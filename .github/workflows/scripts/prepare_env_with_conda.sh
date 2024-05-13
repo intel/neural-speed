5f530c665e2af950871806aa94e68b30fe1cadde
intellinjun <jun.lin@intel.com>
2024-05-13 19:18:59 +0800

    add lm_eval in conda_env
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- .github/workflows/scripts/prepare_env_with_conda.sh
+++ .github/workflows/scripts/prepare_env_with_conda.sh
@@ -12,6 +12,7 @@ pip install -U pip
 
 if [ -f "requirements.txt" ]; then
     python -m pip install --default-timeout=100 -r requirements.txt
+    pip install lm_eval
     pip list
 else
     echo "Not found requirements.txt file."
