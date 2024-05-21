451aa2deee3c903c358a9f87c298b42305f136ca
luoyu-intel <yu.luo@intel.com>
2024-05-21 09:54:26 +0800

    fix UT

--- bestla/bestla/ut/bestla_prologue_b.cpp
+++ bestla/bestla/ut/bestla_prologue_b.cpp
@@ -219,8 +219,7 @@ class UT_BlockQunatize_SN {
 };
 #ifdef BTLA_UT_PROLOGUE_B
 // no proper threshold for this UT
-//
-static UT_BlockQunatize_SN sUT_BlockQunatize_SN;
+// static UT_BlockQunatize_SN sUT_BlockQunatize_SN;
 #endif
 
 class UT_S3_WOQ {
