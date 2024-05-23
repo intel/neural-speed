08a2ae44bc4d017d53ce48d2cd3f138efc74fffe
intellinjun <105184542+intellinjun@users.noreply.github.com>
2024-05-23 15:01:49 +0800

    Update cpp_graph_inference.sh

--- tests/model-test/cpp_graph_inference.sh
+++ tests/model-test/cpp_graph_inference.sh
@@ -464,13 +464,14 @@ function main() {
                             $infer_cmd --seed 1234 -t $cores_per_instance -b 2047 -c $real_ctx -n ${output} -m ${model}-${precision}.bin $extension -p "$prompt" 2>&1 | tee ${WORKSPACE}/${logs_file} || true &
                         fi
                         monitor
+                        chmod 777 ${WORKSPACE}/${logs_file}
                         if [[ ${input} == "1024" || ${cores_per_instance} == "32" ]]; then
                             echo "-------- Accuracy start--------"
                             if [[ "${model}" == "llama"* || "${model}" == "gptj-6b" ]]; then
                                 python ./scripts/cal_acc.py --model_name ${model_path} --init_from_bin ${model}-${precision}.bin --batch_size 8 --tasks lambada_openai,piqa  2>&1 | tee -a ${WORKING_DIR}/${logs_file}
-                            # else
-                            #     python ./scripts/cal_acc.py --model_name ${model_path} --init_from_bin ${model}-${precision}.bin --tasks lambada_openai,boolq,piqa,hellaswag --batch_size 1  2>&1 | tee -a ${WORKING_DIR}/${logs_file}
-                             fi
+                            else
+                                python ./scripts/cal_acc.py --model_name ${model_path} --init_from_bin ${model}-${precision}.bin --tasks lambada_openai,boolq,piqa,hellaswag --batch_size 1  2>&1 | tee -a ${WORKING_DIR}/${logs_file}
+                            fi
                         else
                             echo "-------- Accuracy End --------"
                         fi
