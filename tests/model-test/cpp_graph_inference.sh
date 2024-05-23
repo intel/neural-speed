3368fea2a8850fd5f30250b9d7f6573cca152db6
intellinjun <105184542+intellinjun@users.noreply.github.com>
2024-05-23 13:45:07 +0800

    Update cpp_graph_inference.sh

--- tests/model-test/cpp_graph_inference.sh
+++ tests/model-test/cpp_graph_inference.sh
@@ -470,7 +470,7 @@ function main() {
                                 python ./scripts/cal_acc.py --model_name ${model_path} --init_from_bin ${model}-${precision}.bin --batch_size 8 --tasks lambada_openai,piqa  2>&1 | tee -a ${WORKING_DIR}/${logs_file}
                             # else
                             #     python ./scripts/cal_acc.py --model_name ${model_path} --init_from_bin ${model}-${precision}.bin --tasks lambada_openai,boolq,piqa,hellaswag --batch_size 1  2>&1 | tee -a ${WORKING_DIR}/${logs_file}
-                            # fi
+                             fi
                         else
                             echo "-------- Accuracy End --------"
                         fi
