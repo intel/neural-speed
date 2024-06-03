ac41e40f6c74a5be841f16e7bbd8c5e9e946168b
intellinjun <jun.lin@intel.com>
2024-06-03 16:28:09 +0800

    fix proxy error
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- .github/workflows/scripts/prepare_env_with_conda.bat
+++ .github/workflows/scripts/prepare_env_with_conda.bat
@@ -16,10 +16,10 @@ CALL pip uninstall neural-speed -y
 echo "pip list all the components------------->"
 CALL pip list
 CALL pip config set global.proxy proxy-prc.intel.com:913
-CALL pip install -U pip --proxy=proxy-prc.intel.com:913
+CALL pip install -U pip
 echo "Installing requirements for validation scripts..."
-CALL pip install  -i https://pypi.python.org/simple setuptools_scm --proxy=proxy-prc.intel.com:913
-CALL pip install -r requirements.txt --proxy=proxy-prc.intel.com:913
+CALL pip install  -i https://pypi.python.org/simple setuptools_scm
+CALL pip install -r requirements.txt
 echo "pip list all the components------------->"
 CALL pip list
 echo "------------------------------------------"
