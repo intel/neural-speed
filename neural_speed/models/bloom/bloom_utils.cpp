f112910284fabddc02d01a57c94579ceb3b1d01c
intellinjun <jun.lin@intel.com>
2024-06-06 16:06:42 +0800

    enable bloom mha fusion
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- neural_speed/models/bloom/bloom_utils.cpp
+++ neural_speed/models/bloom/bloom_utils.cpp
@@ -45,6 +45,8 @@ void model_load_internal(const std::string& fname, model_archs arch, model_conte
   std::unique_ptr<BLOOM> ms(new BLOOM());
   ms->init(fname.c_str(), ctx, n_gpu_layers, use_mmap, use_mlock, vocab_only);
   ms->load(ctx, progress_callback, progress_callback_user_data);
+  model_context& lctx = *ctx;
+  lctx.support_bestla_kv = true;
 }
 
 void BLOOM::init(const char* path_model, model_context* ctx, int n_gpu_layer_, bool use_mmap_, bool use_mlock_,
