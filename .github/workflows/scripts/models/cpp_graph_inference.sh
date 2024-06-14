e9b2b9d346a9fd0e9f8d24d51335bf24632a98a7
intellinjun <jun.lin@intel.com>
2024-06-14 17:07:04 +0800

    update ci
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- .github/workflows/scripts/models/cpp_graph_inference.sh
+++ .github/workflows/scripts/models/cpp_graph_inference.sh
@@ -80,7 +80,7 @@ function main() {
 
     ## prepare example requirement
     pip install -r neural_speed/models/requirements/common.txt
-    export LD_LIBRARY_PATH=${HOME}/miniconda3/envs/${conda_env}/lib/:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${HOME}/miniforge3/envs/${conda_env}/lib/:$LD_LIBRARY_PATH
     ## prepare fp32 bin
     python ${convert_script} --outtype f32 --outfile ${working_dir}/${model}-fp32.bin ${input_model}
 
