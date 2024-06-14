96d3134a57c65be7ff28a2f3091b42d5ce36ad21
intellinjun <jun.lin@intel.com>
2024-06-14 17:33:25 +0800

    update ci
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- .github/workflows/scripts/prepare_env_with_conda.sh
+++ .github/workflows/scripts/prepare_env_with_conda.sh
@@ -5,7 +5,7 @@ if [[ -z "${conda_env_name}" ]] || [[ -z "${python_version}" ]]; then
     $BOLD_RED && echo "need provide with conda env name and python version" && $RESET
     exit 1
 fi
-
+source ~/.bashrc 
 conda create -n ${conda_env_name} python=${python_version} -y
 source activate ${conda_env_name} || conda activate ${conda_env_name}
 pip install -U pip
