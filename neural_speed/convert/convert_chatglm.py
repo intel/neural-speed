7cc2a872caa9666460a77db63fc4dd4790e5db71
intellinjun <105184542+intellinjun@users.noreply.github.com>
2024-06-14 15:17:32 +0800

    fix glm4 acc error (#294)
    Signed-off-by: intellinjun <jun.lin@intel.com>

--- neural_speed/convert/convert_chatglm.py
+++ neural_speed/convert/convert_chatglm.py
@@ -561,7 +561,7 @@ def chatglm4_convert(model, tokenizer, dir_model, fname_out, ftype, hparams):
     fout.write(struct.pack("i", 0)) # n_embd_head_k for gemma
     fout.write(struct.pack("f", hparams.get("layernorm_epsilon", 1e-5)))  # rms_norm_eps or layer_norm_eps
     fout.write(struct.pack("f", 10000.0))  # freq_base
-    fout.write(struct.pack("f", hparams.get("rope_ratio", 1)))  # rope_factor
+    fout.write(struct.pack("f", 1))  # rope_factor
 
     fout.write(struct.pack("f", 0.0))  # config.json "rope_scaling.factor", not enabled
     fout.write(struct.pack("i", 0))  # rope_scaling.original_max_position_embeddings
